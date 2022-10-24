eb69d4062c518eeee750cefca1431911bab9ea27
fossephate <matt.cfosse@gmail.com>
2022-10-24 20:47:45 +0000

    fix

--- src/components/StoreLinks.jsx
+++ src/components/StoreLinks.jsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 
 export function StoreLinks() {
   return (
     <div className="btn-container flex flex-row justify-between w-64">
-      <a href="/promo"><img src="https://imagedelivery.net/uA65-M4gr037oB0C4RNdvw/c6fef9c3-dc48-4973-8303-c4d35e655d00/public" /></a>
-      <a href="/promo"><img src="https://imagedelivery.net/uA65-M4gr037oB0C4RNdvw/6c37de35-3ef8-4dd1-ba97-62d5b57fea00/public" /></a>
+      <Link href="/promo"><img src="https://imagedelivery.net/uA65-M4gr037oB0C4RNdvw/c6fef9c3-dc48-4973-8303-c4d35e655d00/public" /></Link>
+      <Link href="/promo"><img src="https://imagedelivery.net/uA65-M4gr037oB0C4RNdvw/6c37de35-3ef8-4dd1-ba97-62d5b57fea00/public" /></Link>
     </div>
   );
 }
